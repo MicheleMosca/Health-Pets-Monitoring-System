020e58bdddccfe6014af8854c7c3cc20e0a35c07
MicheleMosca <michele.mosca026@gmail.com>
2023-02-10 20:26:08 +0100

    Form now are correct

--- Cloud/frontend/src/components/pages/LoginPage.js
+++ Cloud/frontend/src/components/pages/LoginPage.js
@@ -3,67 +3,64 @@ import { Link , useNavigate } from 'react-router-dom'
 
 import '../../App.css'
 
-export default function SignInPage() {
-
-const [username, setUsername] = useState/*<string>*/("");       
-const [password, setPassword] = useState/*<string>*/(""); 
-const [data,setData]=useState(""); 
-const [authenticated, setauthenticated] = useState(localStorage.getItem(localStorage.getItem("authenticated")|| false));
-const navigate = useNavigate();
-
- 
-  window.onload= () => {
-    console.log("Pulisco storage");
-    localStorage.clear(); //pulisco local storage
-  }
+export default function SignInPage()
+{
+    const [username, setUsername] = useState/*<string>*/("");
+    const [password, setPassword] = useState/*<string>*/("");
+    const [data,setData]=useState("");
+    const [authenticated, setauthenticated] = useState(localStorage.getItem(localStorage.getItem("authenticated")|| false));
+    const navigate = useNavigate();
 
 
-const handleLogin = () => {
- 
-    console.log("Dentro Handle Login");
-
-    const loginData = {
-        "username": username,
-        "password": password
-    }
-    console.log("Ecco i dati del login " + JSON.stringify(loginData))
-    console.log("Ecco data  " + JSON.stringify(data))
+      window.onload= () => {
+        console.log("Pulisco storage");
+        localStorage.clear(); //pulisco local storage
+      }
 
 
-      fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(loginData)
-      }).then( (response) => { 
-        if(!response.ok) throw new Error(response.status);
-        else {
-            localStorage.setItem("authenticated", true);
-            setauthenticated(true)
-            return response.text();
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("Dentro Handle Login");
+
+        const loginData = {
+            "username": username,
+            "password": password
         }
-      })  //fixare con status code 
-      .then((res) => { 
-         console.log("Risposta data" + res);
-         localStorage.setItem("auth_token",res);
-         localStorage.setItem("username",username)   
-         setData(res)
-         if(localStorage.getItem("authenticated"))
-            navigate("/home");
-      }).catch((err) => {
-         console.log(err.message);
-      });
-      console.log("finito richiesta");
-      console.log("Ecco il token trovato " + data)
-
-  };
+        console.log("Ecco i dati del login " + JSON.stringify(loginData))
+        console.log("Ecco data  " + JSON.stringify(data))
 
 
+          fetch('/api/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(loginData)
+          }).then( (response) => {
+            if(!response.ok) throw new Error(response.status);
+            else {
+                localStorage.setItem("authenticated", true);
+                setauthenticated(true)
+                return response.text();
+            }
+          })  //fixare con status code
+          .then((res) => {
+             console.log("Risposta data" + res);
+             localStorage.setItem("auth_token",res);
+             localStorage.setItem("username",username)
+             setData(res)
+             if(localStorage.getItem("authenticated"))
+                navigate("/home");
+          }).catch((err) => {
+             console.log(err.message);
+          });
+          console.log("finito richiesta");
+          console.log("Ecco il token trovato " + data)
 
-    
+      };
+
     return (
         <div className="text-center m-5-auto">
             <h2>Sign in to us</h2>
-            <div id='loginForm' /*onSubmit={handleLogin}*/>
+            <form onSubmit={handleLogin}>
                 <p>
                     <label>Username</label><br/>
                     <input value={username}  type="text" name="first_name" required onChange={event => setUsername(event.target.value)} />
@@ -75,9 +72,9 @@ const handleLogin = () => {
                     <input value={password} type="password" name="password" required onChange={event => setPassword(event.target.value)} />
                 </p>
                 <p>
-                    <button id="sub_btn" type="submit"  onClick={handleLogin} >Login</button>
+                    <button id="sub_btn" type="submit"  /*onClick={handleLogin}*/ >Login</button>
                 </p>
-            </div>
+            </form>
             <footer>
                 <p>First time? <Link to="/register">Create an account</Link>.</p>
                 <p><Link to="/">Back to Homepage</Link>.</p>
