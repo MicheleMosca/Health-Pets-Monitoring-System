d29b22d3c64b7e2be3d8629b1e954d84ab0f6963
MicheleMosca <michele.mosca026@gmail.com>
2023-02-13 17:43:00 +0100

    small changes

--- Cloud/frontend/src/components/pages/HomePage.js
+++ Cloud/frontend/src/components/pages/HomePage.js
@@ -1,10 +1,12 @@
-import {React,useEffect} from 'react'
+import {React, useEffect, useState} from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import { Button,Container,Row,Col } from 'react-bootstrap';
 import NavBarComponent from './NavBarComponent'
 
 export default function HomePage() {
     const navigate = useNavigate();
+    const [stations, setStations] = useState();
+    const [animals, setAnimals] = useState();
 
     useEffect(()=>{
         console.log("Sono dentro homepage e localstorage vale" + localStorage.getItem("authenticated"))
@@ -13,7 +15,40 @@ export default function HomePage() {
             navigate("/login");
             console.log("fatto")
         }
-      });
+
+        fetch('/api/users/' + localStorage.getItem('username') + '/stations',
+            {
+                method: 'GET',
+                headers:
+                    {
+                        'X-API-KEY' : localStorage.getItem('auth_token'),
+                        'Content-Type' : 'application/json'
+                    }
+            }).then((response) => {
+            if(!response.ok) throw new Error(response.status);
+            return response.json();
+        }).then((myJson) => {
+            setStations(myJson);
+        })
+
+        for (let i = 0; i < stations.lenght; i++)
+        {
+            fetch('/api/users/' + localStorage.getItem('username') + '/stations/' + stations[i]['id'] + '/animals',
+                {
+                    method: 'GET',
+                    headers:
+                        {
+                            'X-API-KEY' : localStorage.getItem('auth_token'),
+                            'Content-Type' : 'application/json'
+                        }
+                }).then((response) => {
+                if(!response.ok) throw new Error(response.status);
+                return response.json();
+            }).then((myJson) => {
+                setAnimals(myJson);
+            })
+        }
+      }, []);
 
     return (
         <div>
@@ -32,6 +67,12 @@ export default function HomePage() {
                 <Col>2 of 3</Col>
                 <Col>3 of 3</Col>
                 </Row>
+
+                {/*{stations.map( station => (*/}
+                {/*    <p id={station['id']}> {station['id']} </p>*/}
+                {/*))}*/}
+                {}
+                {JSON.stringify(stations)}
             </Container>
             </div>
         </div>
