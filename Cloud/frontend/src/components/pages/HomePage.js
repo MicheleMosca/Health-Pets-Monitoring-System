6f9d7c9ace7b9326dabcce648e12ebf0db244ef3
elmetto97 <elmehdi.elafia@gmail.com>
2023-02-13 15:39:23 +0100

    Added NavBar and bootstrap

--- Cloud/frontend/src/components/pages/HomePage.js
+++ Cloud/frontend/src/components/pages/HomePage.js
@@ -1,5 +1,7 @@
 import {React,useEffect} from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import { Button,Container,Row,Col } from 'react-bootstrap';
+import NavBarComponent from './NavBarComponent'
 
 export default function HomePage() {
     const navigate = useNavigate();
@@ -14,11 +16,25 @@ export default function HomePage() {
       });
 
     return (
-        <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
-            <Link to="/login">
-                <button className="primary-button">Log out</button>
-            </Link>
+        <div>
+            <NavBarComponent></NavBarComponent>
+            <div className="text-center">
+                <h1 className="main-title home-page-title">welcome to our app</h1>
+               
+                <br></br>
+            <Container>
+                <Row>
+                <Col>1 of 2</Col>
+                <Col>2 of 2</Col>
+                </Row>
+                <Row>
+                <Col>1 of 3</Col>
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+                </Row>
+            </Container>
+            </div>
         </div>
+        
     )
 }
