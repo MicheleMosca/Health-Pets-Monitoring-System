59b7f9cbb2fe96338964f82d5b211660fde7b82c
MicheleMosca <michele.mosca026@gmail.com>
2023-02-17 21:58:45 +0100

    Navbar adjustment

--- Cloud/frontend/src/components/pages/NavBarComponent.js
+++ Cloud/frontend/src/components/pages/NavBarComponent.js
@@ -10,24 +10,24 @@ export default function NavBarComponent() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/allStations">All Stations</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+            {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
+            {/*  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#action/3.2">*/}
+            {/*    Another action*/}
+            {/*  </NavDropdown.Item>*/}
+            {/*  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
+            {/*  <NavDropdown.Divider />*/}
+            {/*  <NavDropdown.Item href="#action/3.4">*/}
+            {/*    Separated link*/}
+            {/*  </NavDropdown.Item>*/}
+            {/*</NavDropdown>*/}
           </Nav>
          
           
         </Navbar.Collapse>
         <Nav className="justify-content-end">
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            {/*<Nav.Link href="#deets">More deets</Nav.Link>*/}
             <Nav.Link eventKey={2} href="/login"> Logout {/* <Link to="/login">Log out</Link> */}</Nav.Link>
            
           </Nav>
