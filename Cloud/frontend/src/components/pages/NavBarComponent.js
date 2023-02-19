f984628580b3dc0990bd07b2c01a8f14f86c32b5
MicheleMosca <michele.mosca026@gmail.com>
2023-02-19 17:57:20 +0100

    Small update

--- Cloud/frontend/src/components/pages/NavBarComponent.js
+++ Cloud/frontend/src/components/pages/NavBarComponent.js
@@ -11,27 +11,11 @@ export default function NavBarComponent() {
           <Nav className="me-auto">
             <Nav.Link href="/allStations">All Stations</Nav.Link>
               <Nav.Link href="https://t.me/HealthPetsMonitoringSystem_bot">Telegram Bot</Nav.Link>
-            {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
-            {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
-            {/*  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
-            {/*  <NavDropdown.Item href="#action/3.2">*/}
-            {/*    Another action*/}
-            {/*  </NavDropdown.Item>*/}
-            {/*  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
-            {/*  <NavDropdown.Divider />*/}
-            {/*  <NavDropdown.Item href="#action/3.4">*/}
-            {/*    Separated link*/}
-            {/*  </NavDropdown.Item>*/}
-            {/*</NavDropdown>*/}
           </Nav>
-         
-          
-        </Navbar.Collapse>
-        <Nav className="justify-content-end">
-            {/*<Nav.Link href="#deets">More deets</Nav.Link>*/}
+            <Nav className="justify-content-end">
             <Nav.Link eventKey={2} href="/login"> Logout {/* <Link to="/login">Log out</Link> */}</Nav.Link>
-           
           </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
