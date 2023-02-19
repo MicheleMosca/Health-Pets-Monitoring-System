57dbdb06674164b16f9858304ebcf2c4f90a2602
MicheleMosca <michele.mosca026@gmail.com>
2023-02-19 17:53:04 +0100

    Small update

--- Cloud/frontend/src/components/pages/AllStations.js
+++ Cloud/frontend/src/components/pages/AllStations.js
@@ -17,11 +17,23 @@ export default function AllStations()
     }
     
     return(
-        <div className="text-center">
+        <div className="">
             <div className='text-left mt-3 ml-5'>
                 <button className="text-right" type="button" class="btn btn-secondary" onClick={handleBackButton}>Back</button>
             </div>
-            <h1 className="title">All Our Stations</h1>
+            <h1 className="title text-center">All Our Stations</h1>
+            <div className="container mt-5">
+                <h3 className="">Legend:</h3><br/>
+                <p>
+                    <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" width="25" height="41"/> Alarmed stations
+                    <br/>
+                    <br/>
+                    <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png" width="25" height="41"/> Normal stations
+                    <br/>
+                    <br/>
+                    <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png" width="25" height="41"/> Your Station
+                </p>
+            </div>
             <ShowStations/>
         </div>
     )
--- Cloud/frontend/src/components/pages/NavBarComponent.js
+++ Cloud/frontend/src/components/pages/NavBarComponent.js
@@ -10,6 +10,7 @@ export default function NavBarComponent() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/allStations">All Stations</Nav.Link>
+              <Nav.Link href="https://t.me/HealthPetsMonitoringSystem_bot">Telegram Bot</Nav.Link>
             {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
             {/*<NavDropdown title="Dropdown" id="collasible-nav-dropdown">*/}
             {/*  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
